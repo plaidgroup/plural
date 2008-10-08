40b4be7aa03f00be7af1b9cc528c8bd6a3880f18
kevin.bierhoff <kevin.bierhoff@b3114acd-d154-0410-b278-6fa318000468>
2008-10-08 20:11:22 +0000

    look for @NoEffects in super-types

--- Plural/src/edu/cmu/cs/plural/track/EffectChecker.java
+++ Plural/src/edu/cmu/cs/plural/track/EffectChecker.java
@@ -37,8 +37,12 @@
  */
 package edu.cmu.cs.plural.track;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.AnonymousClassDeclaration;
 import org.eclipse.jdt.core.dom.Assignment;
 import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.ConstructorInvocation;
@@ -57,29 +61,118 @@ import org.eclipse.jdt.core.dom.PrefixExpression;
 import org.eclipse.jdt.core.dom.SuperConstructorInvocation;
 import org.eclipse.jdt.core.dom.SuperMethodInvocation;
 import org.eclipse.jdt.core.dom.ThisExpression;
+import org.eclipse.jdt.core.dom.TypeDeclarationStatement;
 
 import edu.cmu.cs.crystal.AbstractCrystalMethodAnalysis;
 
 /**
+ * This is a simple Crystal checker to make sure that methods and constructors
+ * that carry the @NoEffects annotation have in fact no effects.  Any store into memory
+ * counts as an effect, except stores to receiver fields in a constructor.
  * @author Kevin Bierhoff
  * @since Oct 7, 2008
- *
  */
 public class EffectChecker extends AbstractCrystalMethodAnalysis {
 	
 	@Override
 	public void analyzeMethod(MethodDeclaration d) {
-		if(isPure(d.resolveBinding()))
-			d.accept(new PurityVisitor(d.resolveBinding().getDeclaringClass(),
+		IMethodBinding binding = d.resolveBinding();
+		if(isPure(binding) && ! Modifier.isAbstract(binding.getModifiers()))
+			// needs checked
+			d.accept(new PurityVisitor(binding.getDeclaringClass(),
 					d.isConstructor() /* permit receiver assignment in constructor */));
+		// else method can do whatever it wants
 	}
 	
+	/**
+	 * Helper method to report a problem for the given node to the user.
+	 * @param problemDescription
+	 * @param node
+	 */
+	private void reportUserProblem(String problemDescription, ASTNode node) {
+		reporter.reportUserProblem(problemDescription, node, this.getClass().getName());
+	}
+
+	/**
+	 * Determines whether the given method is declared to have no effects.
+	 * @param methodBinding
+	 * @return <code>true</code> if the given method should have no effects,
+	 * <code>false</code> otherwise.
+	 */
 	private boolean isPure(IMethodBinding methodBinding) {
-		boolean pure = analysisInput.getAnnoDB().getSummaryForMethod(methodBinding).
-				getReturn("edu.cmu.cs.plural.annot.NoEffects") != null;
-		return pure;
+		if(analysisInput.getAnnoDB().getSummaryForMethod(methodBinding).
+				getReturn("edu.cmu.cs.plural.annot.NoEffects") != null)
+			return true;
+		
+		if(methodBinding.getDeclaringClass().getSuperclass() == null)
+			// method in java.lang.Object
+			return false;
+		
+		if(methodBinding.isDefaultConstructor()) {
+			// for compiler-generated default constructors, find the invoked super-constructor
+			// this does not work for constructors found in class files b/c those are not
+			// known to be compiler-generated according to isDefaultConstructor javadoc
+			for(IMethodBinding m : methodBinding.getDeclaringClass().getSuperclass().getDeclaredMethods()) {
+				if(m.isConstructor() && m.getParameterTypes().length == 0)
+					return isPure(m);
+			}
+		}
+		
+		if(methodBinding.isConstructor() || Modifier.isStatic(methodBinding.getModifiers()))
+			// constructors and static methods cannot inherit spec's
+			return false;
+
+		// walk super-types to see if the method is declared to be pure somewhere
+		Set<ITypeBinding> seen = new HashSet<ITypeBinding>();
+		seen.add(methodBinding.getDeclaringClass());
+		if(isPure(methodBinding.getDeclaringClass().getSuperclass(), methodBinding, seen))
+			return true;
+		for(ITypeBinding itf : methodBinding.getDeclaringClass().getInterfaces()) {
+			if(isPure(itf, methodBinding, seen))
+				return true;
+		}
+		return false;
 	}
 	
+	/**
+	 * Helper method for walking supertypes looking for @NoEffects annotations.
+	 * @param type
+	 * @param methodBinding
+	 * @param seen Set of types that were already visited and need not be checked again.
+	 * @return <code>true</code> if a method in the given type that the given 
+	 * method overrides is declared to have no effects, <code>false</code> if
+	 * the given type was <code>null</code>, contained in seen, or did not contain
+	 * any methods declared to have no effects that the given one overrides.
+	 */
+	private boolean isPure(ITypeBinding type, IMethodBinding methodBinding,
+			Set<ITypeBinding> seen) {
+		if(type == null || seen.contains(type))
+			return false;
+		seen.add(type);
+		for(IMethodBinding m : type.getDeclaredMethods()) {
+			if(methodBinding.isSubsignature(m)) {
+				if(analysisInput.getAnnoDB().getSummaryForMethod(m).
+						getReturn("edu.cmu.cs.plural.annot.NoEffects") != null)
+					return true;
+			}
+		}
+		if(isPure(type.getSuperclass(), methodBinding, seen))
+			return true;
+		for(ITypeBinding itf : type.getInterfaces()) {
+			if(isPure(itf, methodBinding, seen))
+				return true;
+		}
+		return false;
+	}
+
+	/**
+	 * This visitor finds stores into memory and impure invocations in the visited 
+	 * AST.  It is intended to be called separately for each method body.  
+	 * This visitor does not descend into anonymous inner and local classes,
+	 * but it would descend into top-level and inner classes.    
+	 * @author Kevin Bierhoff
+	 * @since Oct 7, 2008
+	 */
 	private class PurityVisitor extends ASTVisitor {
 		
 		private final ITypeBinding declaringType;
@@ -162,10 +255,9 @@ public class EffectChecker extends AbstractCrystalMethodAnalysis {
 			}
 			
 			if(!receiver || !permitReceiverAssignment) {
-				EffectChecker.this.reporter.reportUserProblem(
+				EffectChecker.this.reportUserProblem(
 						"Assignment in method with no effects forbidden", 
-						assignment, 
-						EffectChecker.this.getName());
+						assignment);
 			}
 		}
 
@@ -216,11 +308,25 @@ public class EffectChecker extends AbstractCrystalMethodAnalysis {
 		
 		private void checkInvocation(IMethodBinding methodBinding, ASTNode invocation) {
 			if(!EffectChecker.this.isPure(methodBinding))
-				EffectChecker.this.reporter.reportUserProblem(
+				EffectChecker.this.reportUserProblem(
 						"Effectful invocation in method with no effects forbidden", 
-						invocation, EffectChecker.this.getName());
+						invocation);
 		}
 		
+		@Override
+		public boolean visit(AnonymousClassDeclaration node) {
+			// do *not* visit anonymous inner classes
+			// methods of inner classes should be visited separately
+			return false;
+		}
+
+		@Override
+		public boolean visit(TypeDeclarationStatement node) {
+			// do *not* visit local type declarations
+			// methods of inner classes should be visited separately
+			return false;
+		}
+
 	}
 
 }
