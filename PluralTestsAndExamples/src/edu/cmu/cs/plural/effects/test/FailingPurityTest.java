d904a8cd3c636f1b2fa804702591f22985907b81
kevin.bierhoff <kevin.bierhoff@b3114acd-d154-0410-b278-6fa318000468>
2008-10-08 17:57:26 +0000

    additional purity tests

--- PluralTestsAndExamples/src/edu/cmu/cs/plural/effects/test/FailingPurityTest.java
+++ PluralTestsAndExamples/src/edu/cmu/cs/plural/effects/test/FailingPurityTest.java
@@ -4,11 +4,23 @@ import edu.cmu.cs.crystal.annotations.FailingTest;
 import edu.cmu.cs.crystal.annotations.UseAnalyses;
 import edu.cmu.cs.plural.annot.NoEffects;
 
-@FailingTest(3)
+@FailingTest(6)
 @UseAnalyses("EffectChecker")
 public class FailingPurityTest {
 	
+	static int global = 0;
 	int val = 0;
+	
+	@NoEffects
+	FailingPurityTest() {
+		global++;
+	}
+	
+	@NoEffects
+	FailingPurityTest(FailingPurityTest original) {
+		original.val = val;
+		val = original.val;
+	}
 
 	@NoEffects
 	int getVal() {
@@ -23,12 +35,21 @@ public class FailingPurityTest {
 	
 	@NoEffects
 	int inc() {
-		return val++;
+		return ++val;
 	}
 	
 	@NoEffects
 	void setVal(int val) {
 		this.val = val;
+	}
+	
+	@NoEffects
+	void clear() {
+		subtract(val);
+	}
+	
+	void subtract(int value) {
+		val = val - value;
 	}
 
 }
