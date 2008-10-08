c54e42bf5836cbbadd8b20461dbb88a7b81eadb2
kevin.bierhoff <kevin.bierhoff@b3114acd-d154-0410-b278-6fa318000468>
2008-10-08 20:18:13 +0000

    simple tests for purity inheritance

--- PluralTestsAndExamples/src/edu/cmu/cs/plural/effects/test/PurityInheritanceTest.java
+++ PluralTestsAndExamples/src/edu/cmu/cs/plural/effects/test/PurityInheritanceTest.java
@@ -37,56 +37,39 @@
  */
 package edu.cmu.cs.plural.effects.test;
 
-import edu.cmu.cs.crystal.annotations.FailingTest;
+import edu.cmu.cs.crystal.annotations.PassingTest;
 import edu.cmu.cs.crystal.annotations.UseAnalyses;
 import edu.cmu.cs.plural.annot.NoEffects;
 
-@FailingTest(6)
+/**
+ * @author Kevin Bierhoff
+ * @since Oct 8, 2008
+ *
+ */
+@PassingTest
 @UseAnalyses("EffectChecker")
-public class FailingPurityTest {
-	
-	static int global = 0;
-	int val = 0;
+public class PurityInheritanceTest {
 	
 	@NoEffects
-	FailingPurityTest() {
-		global++;
+	public void pureInBase() {
+		
 	}
 	
-	@NoEffects
-	FailingPurityTest(FailingPurityTest original) {
-		original.val = val;
-		val = original.val;
+	public void pureInSub() {
+		
 	}
+}
 
-	@NoEffects
-	int getVal() {
-		return val;
+class ConcreteClass extends PurityInheritanceTest {
+	
+	public void pureInBase() {
+		
 	}
 	
 	@NoEffects
-	int calc() {
-		val = val + val;
-		return val;
+	public void pureInSub() {
+		pureInBase();
+		pureInSub();
 	}
 	
-	@NoEffects
-	int inc() {
-		return ++val;
-	}
-	
-	@NoEffects
-	void setVal(int val) {
-		this.val = val;
-	}
-	
-	@NoEffects
-	void clear() {
-		subtract(val);
-	}
-	
-	void subtract(int value) {
-		val = val - value;
-	}
-
 }
