259e96ba32a684f02aeb341e09645c065658252d
nels.beckman <nels.beckman@b3114acd-d154-0410-b278-6fa318000468>
2008-10-08 14:34:26 +0000

    [test] Interesting test, where a object is not thread-shared, then it is, then it's not again.

---
+++ trunk/PluralTestsAndExamples/src/edu/cmu/cs/nimby/test/interesting/ThreadSharedAndBack.java
@@ -0,0 +1,152 @@
+/**
+ * Copyright (C) 2007, 2008 Carnegie Mellon University and others.
+ *
+ * This file is part of Plural.
+ *
+ * Plural is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Plural is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Plural; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking Plural statically or dynamically with other modules is
+ * making a combined work based on Plural. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole
+ * combination.
+ *
+ * In addition, as a special exception, the copyright holders of Plural
+ * give you permission to combine Plural with free software programs or
+ * libraries that are released under the GNU LGPL and with code
+ * included in the standard release of Eclipse under the Eclipse Public
+ * License (or modified versions of such code, with unchanged license).
+ * You may copy and distribute such a system following the terms of the
+ * GNU GPL for Plural and the licenses of the other code concerned.
+ *
+ * Note that people who make modified versions of Plural are not
+ * obligated to grant this special exception for their modified
+ * versions; it is their choice whether to do so. The GNU General
+ * Public License gives permission to release a modified version
+ * without this exception; this exception also makes it possible to
+ * release a modified version which carries forward this exception.
+ */
+
+package edu.cmu.cs.nimby.test.interesting;
+
+import edu.cmu.cs.plural.annot.Capture;
+import edu.cmu.cs.plural.annot.ClassStates;
+import edu.cmu.cs.plural.annot.FalseIndicates;
+import edu.cmu.cs.plural.annot.Perm;
+import edu.cmu.cs.plural.annot.Pure;
+import edu.cmu.cs.plural.annot.Release;
+import edu.cmu.cs.plural.annot.Share;
+import edu.cmu.cs.plural.annot.State;
+import edu.cmu.cs.plural.annot.TrueIndicates;
+import edu.cmu.cs.plural.annot.Unique;
+
+public class ThreadSharedAndBack {
+
+	public static void foo() {
+		PingPong ping_pong = new PingPong();
+		OtherThread other_thread = new OtherThread(ping_pong); // takes share(ping_pong,1/2)
+
+		// Other thread 'captured' the permission to ping pong, so
+		// we will get back exactly 1/2 when we call join.
+		
+		other_thread.start();
+		
+		for( int i=0; i<5; i++ ) {
+			atomic: {
+				if( ping_pong.isPing() ) {
+					ping_pong.pong(); // We atomically know object is PING
+				}
+				else {
+					ping_pong.ping(); // We atomically know object is PONG
+				}
+			}
+		}
+		
+		try {
+			other_thread.myJoin(); // Gives us back share(ping_pong,1/2)
+			ping_pong.requiresUnique();
+		} catch (InterruptedException e) {}
+		
+	}	
+}
+
+@ClassStates({
+	@State(name="PING", inv="isPing == true"),
+	@State(name="PONG", inv="isPing == false")
+})
+class PingPong {
+	
+	private boolean isPing = true; 
+	
+	@Perm(ensures="unique(this!fr) in PING")
+	PingPong() {
+		
+	}
+	
+	@Pure(fieldAccess=true)
+	@TrueIndicates("PING")
+	@FalseIndicates("PONG")
+	boolean isPing() {
+		atomic: {return isPing;}
+	}
+	
+	@Share(fieldAccess=true, requires="PONG", ensures="PING")
+	void ping() {
+		atomic: {this.isPing = true;}
+	}
+	
+	@Share(fieldAccess=true, requires="PING",ensures="PONG")
+	void pong() {
+		atomic:{this.isPing = false;}
+	}
+	
+	@Unique void requiresUnique() {}
+	
+}
+
+@ClassStates(
+    @State(name="alive", inv="share(myPingPong)")
+)
+class OtherThread extends java.lang.Thread {
+	final PingPong myPingPong;
+	
+	@Perm(requires="share(#0)", ensures="unique(this!fr)")
+	OtherThread(@Capture(param="p") PingPong myPingPong) {
+		this.myPingPong = myPingPong;
+	}
+
+	@Override
+	@Unique(fieldAccess=true)
+	public void run() {
+		for( int i=0; i<5; i++ ) {
+			atomic: {
+				if( this.myPingPong.isPing() ) {
+					this.myPingPong.pong();
+				}
+				else {
+					this.myPingPong.ping();
+				}
+			}
+		}
+	}
+
+	@Override
+	@Perm(requires="unique(this)")
+	public synchronized void start() {
+		super.start();
+	}
+	
+	@Release(value="p")
+	public void myJoin() throws InterruptedException {
+		this.join();
+	}
+}
