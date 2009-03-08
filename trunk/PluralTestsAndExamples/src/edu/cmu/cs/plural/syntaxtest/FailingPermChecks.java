2b554cea6ed2e8edc5085865823a392d33b90de7
kevin.bierhoff <kevin.bierhoff@b3114acd-d154-0410-b278-6fa318000468>
2009-03-08 00:50:12 +0000

    separate package for syntax checker tests; new test file for @Perm and @Full etc tests

--- trunk/PluralTestsAndExamples/src/edu/cmu/cs/plural/syntaxtest/FailingPermChecks.java
+++ trunk/PluralTestsAndExamples/src/edu/cmu/cs/plural/syntaxtest/FailingPermChecks.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2007, 2008 Carnegie Mellon University and others.
+ * Copyright (C) 2007-2009 Carnegie Mellon University and others.
  *
  * This file is part of Plural.
  *
@@ -35,23 +35,66 @@
  * without this exception; this exception also makes it possible to
  * release a modified version which carries forward this exception.
  */
-
-package edu.cmu.cs.plural.test;
+package edu.cmu.cs.plural.syntaxtest;
 
 import edu.cmu.cs.crystal.annotations.FailingTest;
 import edu.cmu.cs.crystal.annotations.UseAnalyses;
-import edu.cmu.cs.plural.annot.ClassStates;
+import edu.cmu.cs.plural.annot.Full;
+import edu.cmu.cs.plural.annot.Perm;
 import edu.cmu.cs.plural.annot.PluralAnalysis;
-import edu.cmu.cs.plural.annot.State;
+import edu.cmu.cs.plural.annot.Pure;
+import edu.cmu.cs.plural.annot.ResultUnique;
+import edu.cmu.cs.plural.annot.States;
 
-@FailingTest(2)
+/**
+ * @author Kevin
+ * @since Mar 7, 2009
+ *
+ */
+@FailingTest(6)
 @UseAnalyses(PluralAnalysis.SYNTAX)
-@ClassStates(@State(name="alive", inv="fakeField == true"))
-public class InvariantFieldIsntReal {
+@States(dim = "A", value = {"x", "y"})
+public interface FailingPermChecks {
 
-}
-
-@ClassStates(@State(name="alive", inv="fakeField == true => full(real)"))
-class MoreFakeFields {
-	private Object real;
+	/* @Perm tests */
+	
+	// error: parameter #0 doesn't exist
+	@Perm(requires = "#0 == null")
+	public void noParam();
+	
+	// ok
+	@Perm(requires = "#0 == null")
+	public void oneParam(Object o);
+	
+	// error: cannot mention result in pre-condition
+	@Perm(requires = "result in open")
+	public Object resultInPre();
+	
+	// two state errors: z not in A, A not in x
+	@Perm(requires = "immutable(this!fr, A) in z", ensures = "unique(result, x) in A")
+	FailingPermChecks copyWithWrongStates();
+	
+	// ok
+	@Perm(requires = "immutable(this!fr, A) in x", ensures = "unique(result) in z")
+	FailingPermChecks copy();
+	
+	// ok
+	@Perm(requires = "immutable(this) in z", ensures = "immutable(result, z)")
+	FailingPermChecks identity();
+	
+	
+	/* @Full etc. tests */
+	
+	// error: no result
+	@ResultUnique
+	void noResult();
+	
+	// error: z not in A
+	@Full(guarantee = "A", requires = "x", ensures = "z")
+	void xToUnknown();
+	
+	// ok
+	@Pure(value = "A", requires = "x", ensures = "y")
+	void xToY();
+	
 }
