3dcecd5ebc2206dbd967bf1d6b96c5698c63d08f
kevin.bierhoff <kevin.bierhoff@b3114acd-d154-0410-b278-6fa318000468>
2009-04-02 20:30:00 +0000

    Extended Fugue inheritance example with most methods

--- PluralTestsAndExamples/src/edu/cmu/cs/plural/examples/FugueInheritance.java
+++ PluralTestsAndExamples/src/edu/cmu/cs/plural/examples/FugueInheritance.java
@@ -1,11 +1,14 @@
 package edu.cmu.cs.plural.examples;
 
+import java.io.IOException;
+import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
 import edu.cmu.cs.crystal.annotations.PassingTest;
 import edu.cmu.cs.crystal.annotations.UseAnalyses;
 import edu.cmu.cs.plural.annot.ClassStates;
+import edu.cmu.cs.plural.annot.Perm;
 import edu.cmu.cs.plural.annot.PluralAnalysis;
 import edu.cmu.cs.plural.annot.State;
 import edu.cmu.cs.plural.annot.Unique;
@@ -26,35 +29,85 @@ import edu.cmu.cs.plural.annot.Use;
 @UseAnalyses({PluralAnalysis.SYNTAX, PluralAnalysis.EFFECT, PluralAnalysis.PLURAL})
 public class FugueInheritance {
 	
+	public static final String OPEN = "open";
+	public static final String CLOSED = "closed";
+	
 	@ClassStates({
-		@State(name = "open", inv = "sock != null"),
-		@State(name = "closed", inv = "sock == null")
+		@State(name = OPEN, inv = "sock != null"),
+		@State(name = CLOSED, inv = "sock == null")
 	})
 	class WebPageFetcher {
 		
-		@SuppressWarnings("unused")
-		private Object sock = null;
+		private String domain;
+		private Socket sock = null;
 		
-		@Unique(requires = "closed", ensures = "open", use = Use.FIELDS)
-		public void open() {
-			this.sock = new Object();
+		@Perm(requires = "#0 != null", ensures = "unique(this!fr) in " + CLOSED)
+		public WebPageFetcher(String domain) {
+			this.domain = domain;
+		}
+		
+		@Unique(requires = CLOSED, ensures = OPEN, use = Use.FIELDS)
+		public void open() throws IOException {
+			sock = new Socket(domain, 80);
+		}
+		
+		@Unique(requires = OPEN, ensures = CLOSED, use = Use.FIELDS)
+		public void close() throws IOException {
+			sock.close();
+			sock = null;
+		}
+		
+		@Unique(guarantee = OPEN, use = Use.FIELDS)
+		public String getPage(String path) {
+			// send GET request; receive response
+			return "content";
+		}
+		
+		@Perm(requires = "#0 != null")
+		@Unique(guarantee = CLOSED, use = Use.FIELDS)
+		public void setDomain(String domain) {
+			this.domain = domain;
 		}
 		
 	}
 	
 	@ClassStates({
-		@State(name = "open", inv = "unique(super) in open * cache != null"),
-		@State(name = "closed", inv = "unique(super) in closed * cache == null")
+		@State(name = OPEN, inv = "unique(super) in open * cache != null"),
+		@State(name = CLOSED, inv = "unique(super) in closed * cache == null")
 	})
 	class CachingFetcher extends WebPageFetcher {
 		
-		@SuppressWarnings("unused")
 		private Map<String, String> cache = null;
 		
+		@Perm(requires = "#0 != null", ensures = "unique(this!fr) in " + CLOSED)
+		public CachingFetcher(String url) {
+			super(url);
+		}
+		
 		@Override
-		public void open() {
+		public void open() throws IOException {
 			super.open();
 			cache = new HashMap<String, String>();
+		}
+
+		@Override
+		public void close() throws IOException {
+			super.close();
+			cache = null;
+		}
+
+		@Override
+		public String getPage(String path) {
+			if(cache.containsKey(path))
+				return cache.get(path);
+			String result = super.getPage(path);
+			cache.put(path, result);
+			return result;
+		}
+
+		@Override
+		public void setDomain(String domain) {
+			super.setDomain(domain);
 		}
 		
 	}
