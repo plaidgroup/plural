877b1f14dbe22651182a2235b5dd01647b408a3a
nels.beckman <nels.beckman@b3114acd-d154-0410-b278-6fa318000468>
2009-05-18 20:27:59 +0000

    Bugfix: Issue65
    Test for issue 65. It now passes.

---
+++ PluralTestsAndExamples/src/edu/cmu/cs/plural/test/Issue65Test.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (C) 2007-2009 Carnegie Mellon University and others.
+ *
+ * This file is part of Plural.
+ *
+ * Plural is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Plural is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Plural; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking Plural statically or dynamically with other modules is
+ * making a combined work based on Plural. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole
+ * combination.
+ *
+ * In addition, as a special exception, the copyright holders of Plural
+ * give you permission to combine Plural with free software programs or
+ * libraries that are released under the GNU LGPL and with code
+ * included in the standard release of Eclipse under the Eclipse Public
+ * License (or modified versions of such code, with unchanged license).
+ * You may copy and distribute such a system following the terms of the
+ * GNU GPL for Plural and the licenses of the other code concerned.
+ *
+ * Note that people who make modified versions of Plural are not
+ * obligated to grant this special exception for their modified
+ * versions; it is their choice whether to do so. The GNU General
+ * Public License gives permission to release a modified version
+ * without this exception; this exception also makes it possible to
+ * release a modified version which carries forward this exception.
+ */
+package edu.cmu.cs.plural.test;
+
+import edu.cmu.cs.crystal.annotations.PassingTest;
+import edu.cmu.cs.crystal.annotations.UseAnalyses;
+import edu.cmu.cs.plural.annot.ClassStates;
+import edu.cmu.cs.plural.annot.NoEffects;
+import edu.cmu.cs.plural.annot.Perm;
+import edu.cmu.cs.plural.annot.PluralAnalysis;
+import edu.cmu.cs.plural.annot.Pure;
+import edu.cmu.cs.plural.annot.Pures;
+import edu.cmu.cs.plural.annot.Refine;
+import edu.cmu.cs.plural.annot.State;
+import edu.cmu.cs.plural.annot.States;
+import edu.cmu.cs.plural.annot.Use;
+
+@PassingTest
+@UseAnalyses(PluralAnalysis.PLURAL)
+@Refine({
+	@States(dim="STRUCTURE", value={"STRUCTURESTATE"}),
+	@States(dim="PROTOCOL", value= {"CLOSED", "STILLOPEN"}),
+	@States(refined="STILLOPEN", value={"OPEN", "CLOSING"})
+})
+@ClassStates({
+	@State(name="STRUCTURE", 
+			inv="share(elements) * reject_enqueue_requests == true => full(this,PROTOCOL) in OPEN"),
+})
+public class Issue65Test {
+
+	boolean reject_enqueue_requests = false;
+	
+	LinkedList elements;
+	
+	@Pures({
+		@Pure(guarantee="STRUCTURE", use=Use.FIELDS)
+	})
+	public final boolean is_empty()
+	{	
+		return elements.size() <= 0; // We should be able to pack here! no Error
+	}
+}
+
+@SuppressWarnings("serial")
+class LinkedList extends java.util.LinkedList<Object> {
+
+	@Perm(ensures="unique(this!fr)")
+	public LinkedList() {
+		super();
+	}
+	
+	@Override
+	@NoEffects
+	public int size() {
+		return super.size();
+	}
+}
