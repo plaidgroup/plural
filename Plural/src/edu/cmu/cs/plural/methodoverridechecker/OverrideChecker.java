02dc1fe3152e65d7fadb12df6c9a38d09a21d286
nels.beckman <nels.beckman@b3114acd-d154-0410-b278-6fa318000468>
2009-07-29 20:16:27 +0000

    I am just about to delete all of the override checking code. Minor issue after minor issue made this much too complicated, and now I am giving up!

--- Plural/src/edu/cmu/cs/plural/methodoverridechecker/OverrideChecker.java
+++ Plural/src/edu/cmu/cs/plural/methodoverridechecker/OverrideChecker.java
@@ -42,18 +42,35 @@ import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 
 import edu.cmu.cs.crystal.AbstractCrystalMethodAnalysis;
-import edu.cmu.cs.crystal.util.Lambda3;
+import edu.cmu.cs.crystal.annotations.AnnotationDatabase;
+import edu.cmu.cs.crystal.internal.WorkspaceUtilities;
+import edu.cmu.cs.crystal.tac.ITACAnalysisContext;
+import edu.cmu.cs.crystal.tac.eclipse.CompilationUnitTACs;
+import edu.cmu.cs.crystal.tac.model.Variable;
 import edu.cmu.cs.crystal.util.Option;
+import edu.cmu.cs.crystal.util.Pair;
+import edu.cmu.cs.plural.contexts.InitialLECreator;
+import edu.cmu.cs.plural.contexts.LinearContext;
+import edu.cmu.cs.plural.contexts.PluralContext;
+import edu.cmu.cs.plural.contexts.TensorPluralTupleLE;
+import edu.cmu.cs.plural.fractions.PermissionSetFromAnnotations;
+import edu.cmu.cs.plural.linear.ErrorReportingVisitor;
+import edu.cmu.cs.plural.pred.PredicateMerger;
+import edu.cmu.cs.plural.states.IInvocationCaseInstance;
 import edu.cmu.cs.plural.states.IMethodCaseInstance;
 import edu.cmu.cs.plural.states.IMethodSignature;
-import edu.cmu.cs.plural.states.StateSpaceRepository;
 import edu.cmu.cs.plural.states.MethodCheckingKind;
-import edu.cmu.cs.plural.fractions.PermissionSetFromAnnotations;
+import edu.cmu.cs.plural.states.StateSpaceRepository;
+import edu.cmu.cs.plural.track.FractionAnalysisContext;
 
 /**
  * In Plural, a method that overrides another method or implements a
@@ -100,7 +117,7 @@ public class OverrideChecker extends AbstractCrystalMethodAnalysis {
 		try {
 			checkMethod(d.resolveBinding(), 
 					arrayAppend(super_interfaces, superclass));
-		} catch(MultipleCasesException mce) {
+		} catch(OverridenSpecificationException mce) {
 			this.getReporter().reportUserProblem(mce.getMessage(), d, getName());
 		}
 	}
@@ -133,7 +150,8 @@ public class OverrideChecker extends AbstractCrystalMethodAnalysis {
 	}
 	
 	// Recursive method that kicks off the checking, given super types.
-	private void checkMethod(IMethodBinding d, ITypeBinding[] super_types) throws MultipleCasesException {
+	private void checkMethod(IMethodBinding d, ITypeBinding[] super_types) throws OverridenSpecificationException {
+		
 		// We create just one set of super types, checking them at the end. This
 		// is because if we reach the same interface through multiple paths
 		// we really only want to report those errors one time.
@@ -160,50 +178,157 @@ public class OverrideChecker extends AbstractCrystalMethodAnalysis {
 	 * Perform the overriding check on one method.
 	 */
 	private void checkSingleMethod(IMethodBinding overriding_method, 
-			IMethodBinding overriden_method) throws MultipleCasesException {
+			final IMethodBinding overriden_method) throws OverridenSpecificationException {
 		IMethodSignature overriden_sig = getRepository().getMethodSignature(overriden_method);
 		IMethodSignature overriding_sig = getRepository().getMethodSignature(overriding_method);
 		
 		// I need to think about how to deal with cases, so for now
 		// we only work if both signatures have one case.
 		if( overriden_sig.cases().size() > 1 ) {
-			throw new MultipleCasesException("Overriden method from " + overriden_method.getDeclaringClass() + " " +
+			throw new OverridenSpecificationException("Overriden method from " + overriden_method.getDeclaringClass() + " " +
 					"uses multiple cases, " +
 					"which is not yet supported by this checker.");
 		}
 		
 		if( overriding_sig.cases().size() > 1 ) {
-			throw new MultipleCasesException("Method uses multiple cases which is not yet supported by this checker.");
+			throw new OverridenSpecificationException("Method uses multiple cases which is not yet supported by this checker.");
 		}
 		
-//		IMethodCaseInstance super_perms =
-//			overriden_sig.cases().get(0).createPermissions(MethodCheckingKind.CONSTRUCTOR_IMPL_CUR_IS_VIRTUAL, true, true).getEnsuredParameterPermissions(1)overriden_method;
-//		IMethodCaseInstance sub_perms = 
-//			overriding_sig.cases().get(0).createPermissions(checkingKind, forAnalyzingBody, isSuperCall)
-//		{
-//			// PRE check
-//			// Each permission in the check needs significantly different permissions. 
-//			checkParameters()
-//
-//		}
+		// So we can do it, for now.
+		// Unfortunately, I need a MethodDecl for the overriden_method...
+		MethodDeclaration super_method_decl = findMethodDecl(overriden_method);
+		FakeTACAnalysisContext tacContext = new FakeTACAnalysisContext(super_method_decl);
+		
+		{
+			// PRE check
+			// Each permission in the check needs significantly different permissions.
+			// I don't know what I am doing yet. This is me getting my thoughts down in code.
+			// From the PRE_super I want to create an initial lattice (a PluralContext)
+			IMethodCaseInstance super_instance =
+				overriden_sig.cases().get(0).createPermissions(MethodCheckingKind.METHOD_IMPL_CUR_NOT_VIRTUAL, 
+					true, false);
+			IMethodCaseInstance sub_instance =
+				overriding_sig.cases().get(0).createPermissions(MethodCheckingKind.METHOD_CALL_DYNAMIC_DISPATCH, 
+						false, false);
+			PluralContext ctx = createEntryLattice(super_instance, true, tacContext);
+			
+			// Split off PRE_sub
+			PluralContext split_off_pre_sub =
+				splitOffPre(ctx, sub_instance, super_method_decl, tacContext);
+			String sat =
+				split_off_pre_sub.getLinearContext().dispatch(new ErrorReportingVisitor(){
+
+					@Override
+					public String checkTuple(TensorPluralTupleLE tuple) {
+						if( tuple.isUnsatisfiable() ) {
+							ITypeBinding clazz = overriden_method.getDeclaringClass();
+							return "Pre-condition of overriding method is strong than the " +
+									"pre-condition of the overriden method from class " + clazz + ".";
+						}
+						else 
+							return null;
+					}});
+			if( sat != null ) 
+				throw new OverridenSpecificationException(sat);
+		}
 		// POST check
 		
-		throw new RuntimeException("NYI");
+		//throw new RuntimeException("NYI");
 	}
 
-	private void checkEntailmentParameters(Lambda3<MethodCheckingKind, Boolean, Boolean, HasParameterPermissions> makePerm) {
-		HasParameterPermissions assumptions = 
-			makePerm.call(MethodCheckingKind.METHOD_IMPL_CUR_NOT_VIRTUAL, true, false);
-		HasParameterPermissions propositions =
-			makePerm.call(MethodCheckingKind.METHOD_CALL_DYNAMIC_DISPATCH, false, false);
+	/**
+	 * Split off the pre-condition permissions from the given method case instance
+	 * from this context, and return the resulting context. (We will make a copy so
+	 * that the returned object is not the same.)
+	 * @param ctx Context we are splitting of from.
+	 * @param sub_instance The specification for the method we are splitting off from. (PRE)
+	 * @param super_method_decl The declaration of the super method so that we can find out
+	 * the actual variable names.
+	 * @param tacContext The analysis context for the method declaration, so we can look up
+	 * source variables.
+	 * @return
+	 */
+	private PluralContext splitOffPre(PluralContext ctx,
+			IMethodCaseInstance sub_instance,
+			MethodDeclaration super_method_decl,
+			ITACAnalysisContext tacContext) {
+		PluralContext result = ctx.copy();
+		int param_num = 0;
 		
-		for( int i = 0; i < assumptions.numParameters(); i++ ) {
-			PermissionSetFromAnnotations assumption = assumptions.getParameterPerm(i);
-			//assumption.
+		// Split off each parameter
+		for( Object param_ : super_method_decl.parameters() ) {
+			SingleVariableDeclaration param = (SingleVariableDeclaration)param_;
+			PermissionSetFromAnnotations param_perms = sub_instance.getRequiredParameterPermissions(1);
+			Variable param_var = tacContext.getSourceVariable(param.resolveBinding());
+			result.splitOff(param_var, param_perms);
+			param_num++;
 		}
 		
+		PermissionSetFromAnnotations rcvr_perms = sub_instance.getRequiredReceiverPermissions();
+		result.splitOff(tacContext.getThisVariable(), rcvr_perms);
+		return result;
+	}
+
+	/**
+	 * @param overriden_method
+	 * @return
+	 * @throws OverridenSpecificationException
+	 */
+	private MethodDeclaration findMethodDecl(IMethodBinding overriden_method)
+			throws OverridenSpecificationException {
+		IJavaElement super_method_ = overriden_method.getMethodDeclaration().getJavaElement();
+		MethodDeclaration super_method_decl;
+		
+		OverridenSpecificationException overridenSpecificationException = new OverridenSpecificationException("Overriden method from " + overriden_method.getDeclaringClass() + " " +
+		" cannot be found. Probably a library class with no specification!");
+		if( super_method_ == null || !(super_method_ instanceof IMethod) ) {
+			throw overridenSpecificationException;
+		}
+		else {
+			Option<MethodDeclaration> super_method_decl_ = 
+				WorkspaceUtilities.getMethodDeclFromModel((IMethod)super_method_);
+			if( super_method_decl_.isNone() ) {
+				throw overridenSpecificationException;
+			}
+			else {
+				super_method_decl = super_method_decl_.unwrap();
+			}
+		}
+		return super_method_decl;
+	}
+
+	/**
+	 * Create an entry lattice. In this case, that's really a context lattice that goes on the
+	 * left-hand side of the turnstile. It will have
+	 * @param instance The instance, which has the ability to generate permissions for some method case.
+	 * @param usePre Should we use this entry lattice be created from the pre-condition permissions
+	 * or from the post condition permissions?
+	 * @return The entry lattice itself.
+	 */
+	private PluralContext createEntryLattice(IMethodCaseInstance instance, boolean usePre, 
+			FakeTACAnalysisContext tacContext) {
+		FractionAnalysisContext fractContext = createFractionContextForSuper(instance, tacContext.getCompUnitTAC());
+		PredicateMerger merger = usePre ? instance.getPreconditionMerger() : instance.getPostconditionMerger();	
+		Pair<LinearContext,?> p = InitialLECreator.createInitialMethodLE(merger, tacContext, fractContext);
+		return PluralContext.createLE(p.fst(), tacContext, fractContext);
 	}
 	
+	/**
+	 * @param instance
+	 * @return
+	 */
+	private FractionAnalysisContext createFractionContextForSuper(
+			final IMethodCaseInstance instance, final CompilationUnitTACs compUnit) {
+		return new FractionAnalysisContext() {
+			@Override public boolean assumeVirtualFrame() { return false; }
+			@Override public IInvocationCaseInstance getAnalyzedCase() { return instance; }
+			@Override public StateSpaceRepository getRepository() { return getRepository(); }
+			@Override public AnnotationDatabase getAnnoDB() { return getInput().getAnnoDB(); }
+			@Override public Option<CompilationUnitTACs> getComUnitTACs() { return Option.some(compUnit); }
+			@Override public Option<IProgressMonitor> getProgressMonitor() {	return Option.none(); }
+		};
+	}
+
 	/**
 	 * Find a method in the given class that overrides the given method or
 	 * return NONE if one does not exist.
@@ -222,9 +347,11 @@ public class OverrideChecker extends AbstractCrystalMethodAnalysis {
 	abstract class HasParameterPermissions {
 		abstract PermissionSetFromAnnotations getParameterPerm(int index);
 		abstract int numParameters();
+		abstract IInvocationCaseInstance getInvocationCase();
 	}
 	
 	abstract class HasReceiverPermissions {
 		abstract void getPerms();
+		abstract IInvocationCaseInstance getInvocationCase();
 	}
 } 
