248d8d6d8ec9ee966a5753078c7940eeaa712949
nels.beckman <nels.beckman@b3114acd-d154-0410-b278-6fa318000468>
2009-11-30 19:04:06 +0000

    First two passing instantiation tests!

---
+++ trunk/PluralTestsAndExamples/src/edu/cmu/cs/plural/polymorphism/InstantiationTest2.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2007, 2008 Carnegie Mellon University and others.
+ *
+ * This file is part of Plural.
+ *
+ * Plural is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Plural is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Plural; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking Plural statically or dynamically with other modules is
+ * making a combined work based on Plural. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole
+ * combination.
+ *
+ * In addition, as a special exception, the copyright holders of Plural
+ * give you permission to combine Plural with free software programs or
+ * libraries that are released under the GNU LGPL and with code
+ * included in the standard release of Eclipse under the Eclipse Public
+ * License (or modified versions of such code, with unchanged license).
+ * You may copy and distribute such a system following the terms of the
+ * GNU GPL for Plural and the licenses of the other code concerned.
+ *
+ * Note that people who make modified versions of Plural are not
+ * obligated to grant this special exception for their modified
+ * versions; it is their choice whether to do so. The GNU General
+ * Public License gives permission to release a modified version
+ * without this exception; this exception also makes it possible to
+ * release a modified version which carries forward this exception.
+ */
+package edu.cmu.cs.plural.polymorphism;
+
+import edu.cmu.cs.crystal.annotations.FailingTest;
+import edu.cmu.cs.crystal.annotations.UseAnalyses;
+import edu.cmu.cs.plural.annot.Apply;
+import edu.cmu.cs.plural.annot.PluralAnalysis;
+import edu.cmu.cs.plural.annot.Pure;
+
+@FailingTest
+@UseAnalyses({"PolyInternalChecker",PluralAnalysis.PLURAL})
+public final class InstantiationTest2 {
+	
+	public void foo(@Apply("share") HasParameter hp, @Pure Object o) {
+		hp.foo(o); // ERROR: We need a share and we only have a pure
+	}
+}
