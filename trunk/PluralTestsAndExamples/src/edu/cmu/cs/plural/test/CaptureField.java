2b9fe486195a8baee449d7a57e1ee229477e2e07
kevin.bierhoff <kevin.bierhoff@b3114acd-d154-0410-b278-6fa318000468>
2010-01-20 01:14:17 +0000

    use state guarantee in capturing method to protect against side effects from other operations

--- trunk/PluralTestsAndExamples/src/edu/cmu/cs/plural/test/CaptureField.java
+++ trunk/PluralTestsAndExamples/src/edu/cmu/cs/plural/test/CaptureField.java
@@ -1,14 +1,18 @@
 package edu.cmu.cs.plural.test;
 
+import edu.cmu.cs.crystal.annotations.PassingTest;
+import edu.cmu.cs.crystal.annotations.UseAnalyses;
 import edu.cmu.cs.plural.annot.Capture;
 import edu.cmu.cs.plural.annot.ClassStates;
 import edu.cmu.cs.plural.annot.Perm;
-import edu.cmu.cs.plural.annot.ResultUnique;
+import edu.cmu.cs.plural.annot.PluralAnalysis;
 import edu.cmu.cs.plural.annot.State;
 import edu.cmu.cs.plural.annot.States;
 import edu.cmu.cs.plural.annot.Unique;
 import edu.cmu.cs.plural.annot.Use;
 
+@PassingTest
+@UseAnalyses(PluralAnalysis.PLURAL)
 @States({"init"})
 @ClassStates({@State(name="init",inv="unique(getsCaptured) in Open")})
 public final class CaptureField {
@@ -25,8 +29,7 @@ public final class CaptureField {
 class GetsCaptured {
 	
 	@Capture(param="underlying")
-	@ResultUnique
-	@Unique(requires="Open", returned=false)
+	@Perm(requires = "unique(this, Open)", ensures = "unique(result)")
 	public Capturer createNewCapturer() {
 		return new Capturer();
 	}
